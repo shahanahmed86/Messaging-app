eaba4195568823dbfefdff7f0726c566c8d91b5d
Ahmed Raza <ahmedcoolestguy@gmail.com>
2020-04-09 00:27:38 +0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import './App.css';
+import Flex from "./Components/Flex box/Flex"
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Flex />
+      </div>
+    )
+  };
+}
+
+export default App;
---
+++ src/Components/Flex box/Flex.js
@@ -0,0 +1,171 @@
+import React from 'react';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import './Flex.css'
+import { FaUserCircle } from 'react-icons/fa';
+import { AiFillSetting } from 'react-icons/ai';
+// import { IoIosSend } from 'react-icons/io';
+
+
+class Flex extends React.Component {
+    render() {
+        return (
+            <div style={{ width: '100%' }}>
+                <div className="header">
+                    <div className="button">
+                        <Button variant="contained" color="primary" href="#contained-buttons" className="join">
+                            Join Board
+                        </Button>
+                        <Button variant="contained" color="primary" href="#contained-buttons">
+                            Start New Topic
+                       </Button>
+                    </div>
+                    <div className="Nav">
+                        <FaUserCircle />  Name  <AiFillSetting />
+                    </div>
+                </div>
+
+                <Box display="flex" flexDirection="row" p={1} m={1} bgcolor="background.paper">
+                    <Box className="boxes B1 scrollbar force-overflow" p={1} bgcolor="grey.100">
+                        <p className="Head"> Discussion topic</p>
+                        <table className="Discussion">
+                            <tr>
+                                <th>
+                                    User : Message
+                                    </th>
+                            </tr>
+                            <tr>
+                                <td>
+                                    User : Message
+                                    </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Kareem : Message
+                                    </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Adeel : Message
+                                    </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Faisal: Message
+                                    </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Rehan : Message
+                                    </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Sender :
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                </td>
+                            </tr>
+                        </table>
+                        <input className="type" type="text" placeholder="Type here" />
+                        <button className="enter">Enter</button>
+                        <br />
+                        <button className="board">Board 1</button>
+                        <button>*</button>
+                    </Box>
+
+                    <Box className="boxes B2 scrollbar force-overflow" p={1} bgcolor="grey.100">
+                        <p className="Head">User in Discussion</p>
+                        <table>
+                            <tr>
+                                <th>
+                                    Sender
+                                    </th>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Hyder
+                                    </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Faisal
+                                    </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Rehan
+                                    </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Kareem
+                                    </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Adeel
+                                    </td>
+                            </tr>
+                        </table>
+                    </Box>
+
+                    <Box className="boxes B3" p={1} bgcolor="grey.100">
+                        <p className="Head">Message List</p>
+                        <div className="mesgs msg_history">
+                            <div className="incoming_msg">
+                                <div className="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil" /> </div>
+                                <div className="received_msg">
+                                    <div className="received_withd_msg">
+                                        <p>Test which is a new approach to have all
+                                             solutions</p>
+                                        <span className="time_date"> 11:01 AM    |    June 9</span></div>
+                                </div>
+                            </div>
+                            <div className="outgoing_msg">
+                                <div className="sent_msg">
+                                    <p>Test which is a new approach to have all
+                                        solutions</p>
+                                    <span className="time_date"> 11:01 AM    |    June 9</span> </div>
+                            </div>
+                            <div className="incoming_msg">
+                                <div className="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil" /> </div>
+                                <div className="received_msg">
+                                    <div className="received_withd_msg">
+                                        <p>Test, which is a new approach to have</p>
+                                        <span className="time_date"> 11:01 AM    |    Yesterday</span></div>
+                                </div>
+                            </div>
+                            <div className="outgoing_msg">
+                                <div className="sent_msg">
+                                    <p>Apollo University, Delhi, India Test</p>
+                                    <span className="time_date"> 11:01 AM    |    Today</span> </div>
+                            </div>
+                            <div className="incoming_msg">
+                                <div className="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil" /> </div>
+                                <div className="received_msg">
+                                    <div className="received_withd_msg">
+                                        <p>We work directly with our designers and suppliers,
+                                        and sell direct to you, which means quality, exclusive
+                                             products, at a price anyone can afford.</p>
+                                        <span className="time_date"> 11:01 AM    |    Today</span></div>
+                                </div>
+                            </div>
+                            <div className="type_msg">
+                                <div className="input_msg_write">
+                                    <input type="text" className="write_msg" placeholder="Type a message" />
+                                    <button className="msg_send_btn" type="button"><i className="fa fa-paper-plane-o" aria-hidden="true"></i></button>
+                                </div>
+                            </div>
+                        </div>
+                    </Box>
+                </Box>
+
+            </div >
+        );
+    }
+}
+
+export default Flex;
